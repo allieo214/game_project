2950199875d3432033b34d231dfa67a29c8f6ad6
William Lathrop <willguy21@gmail.com>
2020-05-25 15:33:37 -0700

    render

--- Render.cpp
+++ Render.cpp
@@ -6,20 +6,50 @@
 #include "VecMat.h"
 #include "Background.h"
 #include "Dialogue_Box.h"
-#include "Character.h"
+#include "CharacterGirl.h"
+#include "Text.h"
+#include <ft2build.h>
+
 
 Background background;
 Dialogue_Box dialogue;
-Character character;
+CharacterGirl characterG;
+
+struct conversationNode {
+
+	std::string statement;
+	std::string optionY;
+	std::string optionN;
+	vec3 color;
+	conversationNode* y;
+	conversationNode* n;
+};
+
 // display
-int winW = 3000, winH = 1500;
+int winW = 1920, winH = 1080;
 //vec2 back_pts[4] = { vec2(-1,-1), vec2(-1,1), vec2(1,1), vec2(1,-1) };
 //vec2 points[4] = { vec2(-1,-1), vec2(-1,1), vec2(-0.5, 1), vec2(-0.5, -1) };
 //vec2 char_pts[4] = { vec2(-3, -3), vec2(-3,3), vec2(3, 3), vec2(3,-3) };
 
+//Char Struct
+//struct Character {
+//	unsigned int TextureID;  // ID handle of the glyph texture
+//	glm::ivec2   Size;       // Size of glyph
+//	glm::ivec2   Bearing;    // Offset from baseline to left/top of glyph
+//	unsigned int Advance;    // Offset to advance to next glyph
+//};
+
+
 // interaction
 float objectX = 0, objectY = 0, objectScale = 1;
 float xDown = 0, yDown = 0, oldMouseX = 0, oldMouseY = 0;
+
+
+
+void DisplayText(const char* fontName, const char* text, int y) {
+	SetFont(fontName, 16, 60);  // exact affect of charRes, pixelRes unclear
+	Text(30, y, vec3(0, 0, 0), 10, text);
+}
 
 // Mouse
 
@@ -78,15 +108,32 @@ void Resize(GLFWwindow *w, int width, int height) {
 int main(int ac, char **av) {
 	//init app window and GL context;
 	glfwInit();
-	GLFWwindow *w = glfwCreateWindow(winW, winH, "background", NULL, NULL);
-	glfwSetWindowPos(w, 100, 100);
+	GLFWwindow *w = glfwCreateWindow(winW, winH, "Dating Sim!", NULL, NULL);
+	glfwSetWindowPos(w, 0, 0);
 	glfwMakeContextCurrent(w);
 	gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
 
-	background.initialize(0, "C:/Users/allis/Graphics/Textures/sci_fi_background2.tga");
+	background.initialize(0, "sci_fi_background2.tga");
 	//background.SetPoints();
-	dialogue.d_initialize(1, "C:/Users/allis/Graphics/Textures/dialogue_test.tga", "C:/Users/allis/Graphics/Textures/dialogue_testMAT.tga");
-	character.initialize(2, "C:/Users/allis/Graphics/Textures/character1.tga", "C:/Users/allis/Graphics/Textures/character1_MAT.tga");
+	dialogue.d_initialize(1, "dialogue_TEST.tga", "dialogue_testMAT.tga");
+	characterG.initialize(2, "character1.tga", "character1_MAT.tga");
+
+
+	//FONT
+	//FT_Library ft;
+	//if (FT_Init_FreeType(&ft))
+	//	std::cout << "ERROR::FREETYPE: Could not init FreeType Library" << std::endl;
+
+	//FT_Face face;
+	//if (FT_New_Face(ft, "fonts/arial.ttf", 0, &face))
+	//	std::cout << "ERROR::FREETYPE: Failed to load font" << std::endl;
+	//FT_Set_Pixel_Sizes(face, 0, 48);
+
+	//if (FT_Load_Char(face, 'X', FT_LOAD_RENDER))
+	//	std::cout << "ERROR::FREETYTPE: Failed to load Glyph" << std::endl;
+
+
+	//std::map<char, Character> Characters;
 
 	// event loop
 	glfwSwapInterval(1);
@@ -94,15 +141,16 @@ int main(int ac, char **av) {
 		glEnable(GL_BLEND);
 		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 		background.Display();
-		character.Display();
+		characterG.Display();
 		dialogue.D_Display();
+		DisplayText("C:/Fonts/OpenSans/OpenSans-Bold.ttf", "Hi there! How are you doing?", 80);
 		glFlush();
 		glfwSwapBuffers(w);
 		glfwPollEvents();
 	}
-	// terminate
+	//terminate
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
-	const GLuint names[] = { background.textureName, dialogue.d_textureName, character.c_textureName};
+	const GLuint names[] = { background.textureName, dialogue.d_textureName, characterG.c_textureName};
 	glfwDestroyWindow(w);
 	glfwTerminate();
 
